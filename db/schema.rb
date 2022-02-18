9898fbe4ebbe8d77a0534a57f0eee7769aa441dc
Paul Lemus <paullemus@protonmail.com>
2022-02-17 19:42:19 -0800

    feat: Added Posts Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_030114) do
+ActiveRecord::Schema.define(version: 2022_02_18_033024) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "link"
+    t.integer "votes"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
